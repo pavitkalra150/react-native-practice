3ea918a6344db95d26c2f77406278bfe60452cb9
pavitkalra150 <c0868150@mylambton.ca>
2023-12-20 22:36:21 -0500

    List and scrollview

--- App.js
+++ App.js
@@ -1,57 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Pavit");
-  const [session, setSession] = useState({ number: 6, title: "hello" });
-  const [current, setCurrent] = useState(true);
-  const [value, setValue] = useState(0);
-  const [clicks, setClicks] = useState(0);
-  const onClickHandler = () => {
-    setName("State Updated to Pavit");
-    setSession({ number: 7, title: "hello2" });
-    setCurrent(false);
-  };
-  const onAddClickHandler = () => {
-    setValue(value + 5);
-    setClicks(clicks + 1);
-  };
-  return (
-    <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>
-        This is session {session.number} with {session.title}
-      </Text>
-      <Text style={styles.text}>
-        {current ? "current session" : "next session"}
-      </Text>
-      <Button
-        title="Update State"
-        style={styles.button}
-        onPress={onClickHandler}
-      ></Button>
+  const [Items, setItems] = useState([
+    { key: 1, item: "Item 1" },
+    { key: 2, item: "Item 2" },
+    { key: 3, item: "Item 3" },
+    { key: 4, item: "Item 4" },
+    { key: 5, item: "Item 5" },
+    { key: 6, item: "Item 6" },
+    { key: 7, item: "Item 7" },
+    { key: 8, item: "Item 8" },
+    { key: 44, item: "Item 9" },
+    { key: 68, item: "Item 27" },
+    { key: 0, item: "Item 78" },
+  ]);
 
-      <Text style={styles.text}>{value}</Text>
-      <Button
-        title="Add"
-        style={styles.button}
-        onPress={onAddClickHandler}
-      ></Button>
-      <Text style={styles.text}>You clicked {clicks} times</Text>
-    </View>
+  return (
+    <ScrollView style={styles.body}>
+      {Items.map((object) => {
+        return (
+          <View style={styles.item} key={object.key}>
+            <Text style={styles.text}>{object.item}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
+  },
+  item: {
+    margin: 10,
+    backgroundColor: "#4ae1fa",
     justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    fontSize: "24px",
+    color: "#000000",
+    fontSize: 45,
     fontStyle: "italic",
     margin: 10,
   },
